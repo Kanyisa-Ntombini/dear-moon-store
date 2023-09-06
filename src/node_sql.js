87d039e4353f2266daf6b0c5f6caabf0e671b4b2
Kanyisa Ntombini <kanyisa.ntombini@gmail.com>
2023-09-06 23:49:18 +0200

    set up postgres database

--- src/node_sql.js
+++ src/node_sql.js
@@ -1,31 +1,30 @@
+const { response } = require('express');
 const { pool } = require('./database_config');
 
 const createTable = async () => {
   await pool.query(`
    CREATE TABLE IF NOT EXISTS Visitors (
    id SERIAL PRIMARY KEY,
-   visitor_name VARCHAR(50) NOT NULL,
-   visitor_age INT NOT NULL,
-   date_of_visit DATE NOT NULL,
-   time_of_visit TIME NOT NULL,
-   assistant_name VARCHAR(50) NOT NULL,
-   comments VARCHAR(500) NOT NULL
+   name VARCHAR(50) NOT NULL,
+   email VARCHAR(50) NOT NULL,
+   message VARCHAR(500) NOT NULL
  )`);
   return 'The Visitors table has been created successfully';
 };
 
-const addNewVisitor = async () => {
+const addNewVisitor = async (name, email, message) => {
   await pool.query(
-    `INSERT INTO Visitors (visitor_name, visitor_age, date_of_visit, time_of_visit, assistant_name, comments)
-  VALUES ($1, $2, $3, $4, $5, $6)`,
-    [visitorName, visitorAge, dateOfVisit, timeOfVisit, assistantName, comments]
+    `INSERT INTO Visitors (name, email, message) VALUES ($1, $2, $3)`,
+    [name, email, message]
   );
   return 'A new visitor has been added successfully';
 };
 
 const listAllVisitors = async () => {
   const listOfVisitors = await pool.query(
-    `SELECT id, visitor_name FROM Visitors`
+    `SELECT id, name, email, message FROM Visitors`
   );
   return listOfVisitors.rows;
 };
+
+module.exports = { createTable, addNewVisitor, listAllVisitors };
